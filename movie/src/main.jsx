785f33a6c9e0aac2c633fbf9469da90508365285
Hasnain Shaikh <hasnainshaikh78213@gmail.com>
2025-10-24 23:24:04 -0700

    first commit

---
+++ movie/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+
